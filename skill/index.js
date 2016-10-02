7e7450ab025139ae17b2754eb3404cf2248698d4
Matt DuLeone <matt@duleone.com>
2016-10-02 03:29:08 -0400

    Add the Lambda button skill

---
+++ skill/index.js
@@ -0,0 +1,30 @@
+'use strict';
+var fetch = require('isomorphic-fetch');
+var env = require('./env');
+
+/**
+ * The following JSON template shows what is sent as the payload:
+{
+    "serialNumber": "GXXXXXXXXXXXXXXXXX",
+    "batteryVoltage": "xxmV",
+    "clickType": "SINGLE" | "DOUBLE" | "LONG"
+}
+ *
+ * A "LONG" clickType is sent if the first press lasts longer than 1.5 seconds.
+ * "SINGLE" and "DOUBLE" clickType payloads are sent for short clicks.
+ *
+ * For more documentation, follow the link below.
+ * http://docs.aws.amazon.com/iot/latest/developerguide/iot-lambda-rule.html
+ */
+exports.handler = function(event, context, callback) {
+    console.log('Received event:', event.clickType);
+
+    switch (event.clickType) {
+        case 'SINGLE':
+            return fetch(`${env.homeUrl}/singlePress?batteryVoltage=${event.batteryVoltage}`, {method: 'GET'});
+        case 'DOUBLE':
+            return fetch(`${env.homeUrl}/doublePress?batteryVoltage=${event.batteryVoltage}`, {method: 'GET'});
+        case 'LONG':
+            return fetch(`${env.homeUrl}/longPress?batteryVoltage=${event.batteryVoltage}`, {method: 'GET'});
+    }
+};
